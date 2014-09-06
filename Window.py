cc098598980a6bb192a5e74e352fff55371e783a
ihaddy <ihaddy@gmail.com>
2014-09-06 14:04:47 -0400

    Main Window
    The Main window that the game takes place in

---
+++ Window.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+
+
+
+#Colors
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0,255,0)
+RED   = (255, 0, 0)
+
+#Numeric Constants
+PI = 3.141592653
+
+#Screen parameters
+size = (700,500)
+pygame.display.set_caption("Test")
+
+
+
+screen = pygame.display.set_mode(size)
+
+
+
