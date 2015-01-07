74231a483e49b833dc096d87bd6b573c5cdda7f5
ihaddy <ihaddy@gmail.com>
2015-01-06 20:03:39 -0500

    window code
    misc files added (not sure from where) + refresh code added to window

--- Window.py
+++ Window.py
@@ -23,4 +23,38 @@ pygame.display.set_caption("Test")
 screen = pygame.display.set_mode(size)
 
 
+#Game clock parameters
+done = False
+clock = pygame.time.Clock()
 
+
+while not done:
+    
+    #Event Loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            print("User Exited the program.")
+            pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            print("User Pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User released a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+            
+#Clear Screen
+    screen.fill(WHITE)
+# Draw on the screen several lines from (0, 10) to (100, 110)
+# 5 pixels wide using a while loop
+    y_offset = 0
+    while y_offset < 1000:
+        pygame.draw.line(screen,RED,[0,10+y_offset],[100,110+y_offset],5)
+        y_offset = y_offset + 10
+#Update Screen
+    pygame.display.flip()
+
+    clock.tick(60)
+
+    
+    
